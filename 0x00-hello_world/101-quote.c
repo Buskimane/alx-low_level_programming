ff7a3731c027765a4d6efefeb9c87841c83949cd
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-06-30 12:24:04 -0700

    Ctask3

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	fprint(stderr,"and that piece of art is useful\"-\
+			Dora Korpar, 2015- 10- 19\n");
+
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h
+#include <string.h>
 /**
  * main - Entry point
  *
