f43f534876c5345e1aea167db5d31151d821b092
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-06-30 20:30:43 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	fprint(stderr,"and that piece of art is useful\"-\
-			Dora Korpar, 2015- 10- 19\n");
+fprint(stderr,"and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
 
-	return (1);
+return (1);
 }
