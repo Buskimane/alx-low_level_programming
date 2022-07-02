2d5ca60d5b572334fa79cee345d4ce1ad30057be
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-02 06:03:37 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints a hexadecimal string
- *     *
- *       * Return: Always (Success)
- *         */
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-		char c;
+	char c;
 
-			for (c = '0'; c <= '9'; c++)
-					{
-								putchar(c);
-									}
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
 
-				for (c = 'a'; c <= 'f'; c++)
-						{
-									putchar(c);
-										}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
 
-					putchar('\n');
+	putchar('\n');
 
-						return (0);
+	return (0);
 }
