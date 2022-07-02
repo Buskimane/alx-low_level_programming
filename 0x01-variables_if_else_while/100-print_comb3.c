14369ba9e01e6218337869885e82ad57c1f7d851
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-02 06:05:15 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints combination of numbers
- *     *
- *       * Return: Always (Success)
- *         */
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-		int c, i;
+	int c, i;
 
-			for (c = '0'; c <= '9'; c++)
-					{
-								for (i = '0'; i <= '9'; i++)
-											{
-															if (c < i)
-																			{
-																								putchar(c);
-																												putchar(i);
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
 
-																																if (c != '8' || (c == '8' && i != '9'))
-																																					{
-																																											putchar(',');
-																																																putchar(' ');
-																																																				}
-																																			}
-																	}
-									}
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-				putchar('\n');
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
-
