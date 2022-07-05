21b96f78aa7bbb4dd704ff6d11d02d0e41b20332
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-05 12:05:03 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,34 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *  * jack_bauer - print every minute of the day
- *   *
- *    *
- *     *
- *      * Return: every minute of the day of Jack Bauer
- *       */
+ * jack_bauer - print every minute of the day
+ *
+ *
+ *
+ * Return: every minute of the day of Jack Bauer
+ */
 void jack_bauer(void)
 {
-		int H, h, M, m;
+	int H, h, M, m;
 
-			for (H = 0; H <= 2; H++)
+	for (H = 0; H <= 2; H++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (M = 0; M < 6; M++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					if (!((H == 2) && (h > 3)))
 					{
-								for (h = 0; h <= 9; h++)
-											{
-															for (M = 0; M < 6; M++)
-																			{
-																								for (m = 0; m <= 9; m++)
-																													{
-																																			if (!((H == 2) && (h > 3)))
-																																									{
-																																																_putchar('0' + H);
-																																																						_putchar('0' + h);
-																																																												_putchar(':');
-																																																																		_putchar('0' + M);
-																																																																								_putchar('0' + m);
-																																																																														_putchar('\n');
-																																																																																			}
-																																							}
-																											}
-																	}
-									}
+						_putchar('0' + H);
+						_putchar('0' + h);
+						_putchar(':');
+						_putchar('0' + M);
+						_putchar('0' + m);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
-Footer
-
