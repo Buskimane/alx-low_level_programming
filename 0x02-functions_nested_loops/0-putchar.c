15e31fa3c16aea7173cf8289546214dfa2fb958c
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-05 21:11:20 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,19 +7,11 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 char *sh = "_putchar";
 while (*sh)
 {
 _putchar(*sh);
 sh++;
-=======
-	char *sh = "_putchar";
-	while (*sh)
-{		
-_putchar(*sh);
-sh++;		
->>>>>>> 5967baab61522ae3806aa3e79fa0559715191e1d
 _putchar('\n');
 return (0);
 }
