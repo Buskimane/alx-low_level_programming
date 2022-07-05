175c62d28a54f2ae6e4da8e864995302b3ace30c
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-05 11:58:46 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  *print_alphabet_x10 - 10 times Alphabet in lowercase
- *   *
- *    *Return:0
- *     *
- *      */
+ *print_alphabet_x10 - 10 times Alphabet in lowercase
+ *
+ *Return:0
+ *
+ */
 void print_alphabet_x10(void)
 {
-		char r;
-			int a = 0;
+	char r;
+	int a = 0;
 
-				while (a < 10)
-						{
-									for (r = 'a'; r <= 'z'; r++)
-												{
-																_putchar(r);
-																		}
+	while (a < 10)
+	{
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r);
+		}
 
-											_putchar('\n');
-													a++;
-														}
+		_putchar('\n');
+		a++;
+	}
 }
