e2b1e7c4fcfa713825c2cec4c291b9373eea5718
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-05 12:00:16 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  *_islower - check lowercase character.
- *   *
- *    *@c: integer 1 or 0
- *     *
- *      *Return: 1 lowercase 0 Not
- *       *
- *        */
+ *_islower - check lowercase character.
+ *
+ *@c: integer 1 or 0
+ *
+ *Return: 1 lowercase 0 Not
+ *
+ */
 int _islower(int c)
 {
-		if (c > 96 && c < 123)
-				{
-							return (1);
-								}
-			else
-					{
-								return (0);
-									}
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
