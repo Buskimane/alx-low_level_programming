50eb3c9e0ac6ced0c2a79bf54be76632660dde66
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-07-05 03:51:55 -0700

    Loop 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+include "holberton.h"
+
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
+{
+		char l[] = {"Holberton"};
+			int i;
+
+				for (i = 0; i < 9; i++)
+						{
+									_putchar(l[i]);
+										}
+					_putchar('\n');
+
+						return (0);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *  * add - add two enteros
+ *   * @a: entero
+ *    * @b: entero
+ *     *
+ *      *
+ *       * Return: a + b
+ *        */
+int add(int a, int b)
+{
+		return (a + b);
+}
