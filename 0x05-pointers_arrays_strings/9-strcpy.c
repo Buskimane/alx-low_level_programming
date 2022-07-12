f1fb17f967e643cc35a7b68731dc2b44d23ddb20
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-12 19:37:12 +0100

    Update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string (including the terminating null-byte)
+ *
+ * @dest: the buffer to which the string should be copied
+ * @src: the string to copy
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len = 0;
+int pos = 0;
+
+if (dest && src)
+{
+while (*(src + len))
+++len;
+
+if (src + len < dest || dest + len < src)
+{
+do {
+*(dest + pos++) = *src++;
+} while (len--);
+}
+}
+return (dest);
+}
