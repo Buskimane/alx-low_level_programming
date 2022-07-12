931b8a0c037a3abf726a6cb9d67d98df03565e20
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-12 19:33:39 +0100

    Update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string, followed by a new line
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+if (str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
+}
