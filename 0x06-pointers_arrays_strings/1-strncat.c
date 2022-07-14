fac8cfe98fff1420736ccee719df526e3f3cc45c
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-14 10:00:35 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,16 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int dlen = 0, j = 0;
-
 while (dest[dlen])
 {
 dlen++;
 }
-
 while (j < n && src[j])
 {
 dest[dlen] = src[j];
 dlen++;
 j++;
 }
-
 dest[dlen + n + 1] = '\0';
-
 return (dest);
 }
