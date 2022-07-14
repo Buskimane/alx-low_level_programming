d7a49b89d8c8e4435594c4746373b67b4dc8775a
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-14 14:22:43 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -81,36 +81,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	return (add_strings(n1, n2, r, --size_r));
 }
-if (tens && r_index >= 0)
-{
-*(r + r_index) = (tens % 10) + '0';
-return (r + r_index);
-}
-else if (tens && r_index < 0)
-return (0);
-return (r + r_index + 1);
-}
-/**
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-int index, n1_len = 0, n2_len = 0;
-for (index = 0; *(n1 + index); index++)
-n1_len++;
-for (index = 0; *(n2 + index); index++)
-n2_len++;
-if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-return (0);
-n1 += n1_len - 1;
-n2 += n2_len - 1;
-*(r + size_r) = '\0';
-return (add_strings(n1, n2, r, --size_r));
-}
