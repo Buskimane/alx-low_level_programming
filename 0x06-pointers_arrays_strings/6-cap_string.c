92f61a74ffc1110b19640d48f34809a74deda40e
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-14 13:33:58 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 int a = 0, i;
 int cspc = 13;
 char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
-while (s[a])							
+while (s[a])
 {
 i = 0;
 while (i < cspc)
@@ -25,4 +25,3 @@ a++;
 }
 return (s);
 }
-
