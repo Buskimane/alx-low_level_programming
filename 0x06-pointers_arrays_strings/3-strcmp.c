1c6ee76f40a2d8ec783af89c739f1a8af92a55e2
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-14 13:28:05 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,10 @@
 int _strcmp(char *s1, char *s2)
 {
 int a = 0, b = 0, c = 0, r = 0, lim;
-
 while (s1[a])
 {
 a++;
 }
-
 while (s2[b])
 {
 b++;
@@ -32,13 +30,15 @@ while (c <= lim)
 {
 if (s1[c] == s2[c])
 {
-c++;																	continue;
+c++;
+continue;
 }
 else
 {
 r = s1[c] - s2[c];
 break;
 }
-c++;																	}
+c++;
+}
 return (r);
 }
