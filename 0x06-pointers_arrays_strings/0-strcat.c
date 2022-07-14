6e2142bfe000c370e87e7ecfccf5c0e49540d071
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-14 09:58:41 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,15 @@
 char *_strcat(char *dest, char *src)
 {
 int dlen = 0, i;
-
 while (dest[dlen])
 {
 dlen++;
 }
-
 for (i = 0; src[i] != 0; i++)
 {
 dest[dlen] = src[i];
 dlen++;
 }
-
 dest[dlen] = '\0';
 return (dest);
 }
