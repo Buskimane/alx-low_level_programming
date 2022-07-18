88a32eca56dfd05b45aa58760160293a7271d516
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-07-18 09:55:44 -0700

    last point

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
