2f1e72ddca06f3a1de24f4e569b21ec02f2f317b
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-07-22 14:21:13 -0700

    static

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: the string to calculate the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+if (s)
+{
+while (*(s + len))
+++len;
+}
+return (len);
+}
