cd8fdaf640cd47c3b2432b6c7f37039405bbf7c9
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-07-25 14:56:45 -0700

    Malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,40 +10,40 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *str, *s;
-	int i, j, k, len = 0;
+char *str, *s;
+int i, j, k, len = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		s = av[i];
-		j = 0;
+for (i = 0; i < ac; i++)
+{
+s = av[i];
+j = 0;
 
-		while (s[j++])
-			len++;
-		len++;
-	}
+while (s[j++])
+len++;
+len++;
+}
 
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-		return (NULL);
+str = (char *)malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
 
-	for (i = 0, j = 0; i < ac && j < len; i++)
-	{
-		s = av[i];
-		k = 0;
+for (i = 0, j = 0; i < ac && j < len; i++)
+{
+s = av[i];
+k = 0;
 
-		while (s[k])
-		{
-			str[j] = s[k];
-			k++;
-			j++;
-		}
-		str[j++] = '\n';
-	}
-	str[j] = '\0';
+while (s[k])
+{
+str[j] = s[k];
+k++;
+j++;
+}
+str[j++] = '\n';
+}
+str[j] = '\0';
 
-	return (str);
+return (str);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,36 +12,36 @@ void create_word(char **, char *, int, int, int);
  */
 char **strtow(char *str)
 {
-	int i, flag, len;
-	char **words;
+int i, flag, len;
+char **words;
 
-	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
-		return (NULL);
+if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+return (NULL);
 
-	i = flag = len = 0;
-	while (str[i])
-	{
-		if (flag == 0 && str[i] != ' ')
-			flag = 1;
-		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-		{
-			flag = 0;
-			len++;
-		}
-		i++;
-	}
+i = flag = len = 0;
+while (str[i])
+{
+if (flag == 0 && str[i] != ' ')
+flag = 1;
+if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+{
+flag = 0;
+len++;
+}
+i++;
+}
 
-	len += flag == 1 ? 1 : 0;
-	if (len == 0)
-		return (NULL);
+len += flag == 1 ? 1 : 0;
+if (len == 0)
+return (NULL);
 
-	words = (char **)malloc(sizeof(char *) * (len + 1));
-	if (words == NULL)
-		return (NULL);
+words = (char **)malloc(sizeof(char *) * (len + 1));
+if (words == NULL)
+return (NULL);
 
-	util(words, str);
-	words[len] = NULL;
-	return (words);
+util(words, str);
+words[len] = NULL;
+return (words);
 }
 
 /**
@@ -51,29 +51,29 @@ char **strtow(char *str)
  */
 void util(char **words, char *str)
 {
-	int i, j, start, flag;
+int i, j, start, flag;
 
-	i = j = flag = 0;
-	while (str[i])
-	{
-		if (flag == 0 && str[i] != ' ')
-		{
-			start = i;
-			flag = 1;
-		}
+i = j = flag = 0;
+while (str[i])
+{
+if (flag == 0 && str[i] != ' ')
+{
+start = i;
+flag = 1;
+}
 
-		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-		{
-			create_word(words, str, start, i, j);
-			j++;
-			flag = 0;
-		}
+if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+{
+create_word(words, str, start, i, j);
+j++;
+flag = 0;
+}
 
-		i++;
-	}
+i++;
+}
 
-	if (flag == 1)
-		create_word(words, str, start, i, j);
+if (flag == 1)
+create_word(words, str, start, i, j);
 }
 
 /**
@@ -86,12 +86,12 @@ void util(char **words, char *str)
  */
 void create_word(char **words, char *str, int start, int end, int index)
 {
-	int i, j;
+int i, j;
 
-	i = end - start;
-	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+i = end - start;
+words[index] = (char *)malloc(sizeof(char) * (i + 1));
 
-	for (j = 0; start < end; start++, j++)
-		words[index][j] = str[start];
-	words[index][j] = '\0';
+for (j = 0; start < end; start++, j++)
+words[index][j] = str[start];
+words[index][j] = '\0';
 }
