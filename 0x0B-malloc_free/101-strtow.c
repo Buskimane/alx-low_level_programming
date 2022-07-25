c3d91f082657dc6130193359c6602e3739b75e3d
Buskimane <107367978+Buskimane@users.noreply.github.com>
2022-07-25 23:03:11 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,10 +14,8 @@ char **strtow(char *str)
 {
 int i, flag, len;
 char **words;
-
 if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 return (NULL);
-
 i = flag = len = 0;
 while (str[i])
 {
@@ -30,20 +28,16 @@ len++;
 }
 i++;
 }
-
 len += flag == 1 ? 1 : 0;
 if (len == 0)
 return (NULL);
-
 words = (char **)malloc(sizeof(char *) * (len + 1));
 if (words == NULL)
 return (NULL);
-
 util(words, str);
 words[len] = NULL;
 return (words);
 }
-
 /**
  * util - a util function for fetching words into an array
  * @words: the strings array
@@ -52,7 +46,6 @@ return (words);
 void util(char **words, char *str)
 {
 int i, j, start, flag;
-
 i = j = flag = 0;
 while (str[i])
 {
@@ -61,21 +54,17 @@ if (flag == 0 && str[i] != ' ')
 start = i;
 flag = 1;
 }
-
 if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 {
 create_word(words, str, start, i, j);
 j++;
 flag = 0;
 }
-
 i++;
 }
-
 if (flag == 1)
 create_word(words, str, start, i, j);
 }
-
 /**
  * create_word - creates a word and insert it into the array
  * @words: the array of strings
@@ -87,10 +76,8 @@ create_word(words, str, start, i, j);
 void create_word(char **words, char *str, int start, int end, int index)
 {
 int i, j;
-
 i = end - start;
 words[index] = (char *)malloc(sizeof(char) * (i + 1));
-
 for (j = 0; start < end; start++, j++)
 words[index][j] = str[start];
 words[index][j] = '\0';
