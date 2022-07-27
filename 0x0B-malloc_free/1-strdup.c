04e2bf25f8136d5d3cb77c526e39c27f9a5ed5ce
Adebusola Ayo-Olaoye <adebusolaemmanuel@gmail.com>
2022-07-27 11:37:37 -0700

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,26 +11,20 @@ char *_strdup(char *str)
 {
 int a = 0, i = 1;
 char *s;
-
 if (str == NULL)
 return (NULL);
-
 while (str[i])
 {
 i++;
 }
-
 s = malloc((sizeof(char) * i) + 1);
-
 if (s == NULL)
 return (NULL);
-
 while (a < i)
 {
 s[a] = str[a];
 a++;
 }
-
 s[a] = '\0';
 return (s);
 }
