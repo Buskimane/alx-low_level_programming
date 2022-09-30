11e0cd838406b20024b274e6f29f1c62e919c330
Buskimane <adebusolaemmanuel@gmail.com>
2022-09-30 05:12:42 +0100

    list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,43 @@
+#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to a pointer to a doubly linked list
+ * @idx: index of the list where the new node should be added
+ * @n: integer
  * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+	dlistint_t *current_node = *h;
+	dlistint_t *new_node = NULL;
+	unsigned int i = 0;
 
-	new = NULL;
 	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
+		return (add_dnodeint(h, n));
+	current_node = current_node->next;
+	while (current_node)
 	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
+		i++;
+		if (i == idx)
 		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
+			new_node = malloc(1 * sizeof(dlistint_t));
+			if (!new_node)
+				return (NULL);
+			new_node->prev = current_node->prev;
+			new_node->prev->next = new_node;
+			new_node->next = current_node;
+			new_node->n = n;
+			current_node->prev = new_node;
+			return (new_node);
 		}
+		current_node = current_node->next;
 	}
 
-	return (new);
+	if (idx - i == 1)
+		return (add_dnodeint_end(h, n));
+	return (NULL);
 }
